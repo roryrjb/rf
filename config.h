ac9ae5317f32a6be627a688fd9f2b80122de92df
Rory Bradford <rory@dysfunctionalprogramming.com>
2019-11-15 17:12:07 +0000

    Initial implementation
    Signed-off-by: Rory Bradford <rory@dysfunctionalprogramming.com>

---
+++ config.h
@@ -0,0 +1,11 @@
+char *ignored_dirs[] = {"node_modules", ".mypy_cache", ".git", ".hg",
+                        "__pycache__"};
+
+size_t ignored_dirs_size = sizeof(ignored_dirs) / sizeof(ignored_dirs[0]);
+
+char *ignored_extensions[] = {".pyc", ".jpg", ".jpeg", ".png", ".gif",
+                              ".zip", ".tar", ".xz",   ".gz",  ".gzip",
+                              ".jar", ".apk", ".deb"};
+
+size_t ignored_extensions_size =
+    sizeof(ignored_extensions) / sizeof(ignored_extensions[0]);
