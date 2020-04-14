bba4156d518540d2e939b7e5a06102be56505f52
Rory Bradford <roryrjb@gmail.com>
2020-04-14 15:51:23 +0100

    Update default ignored file extensions
    Signed-off-by: Rory Bradford <roryrjb@gmail.com>

--- config.h
+++ config.h
@@ -3,8 +3,9 @@ char *ignored_dirs[] = {"*node_modules*", "*.mypy_cache*", "*.git*", "*.hg*",
 
 size_t ignored_dirs_size = sizeof(ignored_dirs) / sizeof(ignored_dirs[0]);
 
-char *ignored_extensions[] = {".pyc", ".jpg", ".jpeg", ".png", ".gif", ".zip",
-	".tar", ".xz", ".gz", ".gzip", ".jar", ".apk", ".deb", ".o"};
+char *ignored_extensions[] = {"*.pyc", "*.jpg", "*.jpeg", "*.png", "*.gif",
+	"*.zip", "*.tar", "*.xz", "*.gz", "*.gzip", "*.jar", "*.apk", "*.deb",
+	"*.o"};
 
 size_t ignored_extensions_size =
 	sizeof(ignored_extensions) / sizeof(ignored_extensions[0]);
